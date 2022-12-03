e0941245ff1d2aee56df425e70217bd0da0d972c
Marcos Visentini <marcosvisentini@gmail.com>
2022-12-03 15:00:09 -0300

    Set up Spring Boot project using Spring Initializr

---
+++ src/main/java/br/dev/wisentini/featuretoggle/FeatureToggleApplication.java
@@ -0,0 +1,13 @@
+package br.dev.wisentini.featuretoggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureToggleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureToggleApplication.class, args);
+	}
+
+}
