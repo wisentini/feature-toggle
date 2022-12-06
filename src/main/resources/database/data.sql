c6590928e413f1c61d75ae5f4f0c990ffef95e00
Marcos Visentini <marcosvisentini@gmail.com>
2022-12-05 22:28:32 -0300

    Add data and schema SQL scripts so that Spring automatically loads initial data

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO "user" (name, password) VALUES ('Leonardo', 'banana5');
+INSERT INTO "user" (name, password) VALUES ('Guilherme', 'maçã9');
+INSERT INTO "user" (name, password) VALUES ('David', 'uva1');
+INSERT INTO "user" (name, password) VALUES ('Marcos', 'laranja4');
+INSERT INTO "user" (name, password) VALUES ('Luana', 'pêssego7');
+INSERT INTO "user" (name, password) VALUES ('Jean', 'morango3');
+INSERT INTO "user" (name, password) VALUES ('Mateus', 'mamão2');
+INSERT INTO "user" (name, password) VALUES ('Henrique', 'melancia6');
+INSERT INTO "user" (name, password) VALUES ('Carol', 'manga10');
+INSERT INTO "user" (name, password) VALUES ('Maria', 'melão8');
