d9df7e9e113fb802286a9d93951356107a60af9f
Marcos Visentini <marcosvisentini@gmail.com>
2022-12-17 09:59:23 -0300

    Add validation method to UserCreationDTO class

--- src/main/java/br/dev/wisentini/featuretoggle/dto/UserCreationDTO.java
+++ src/main/java/br/dev/wisentini/featuretoggle/dto/UserCreationDTO.java
@@ -1,6 +1,10 @@
 package br.dev.wisentini.featuretoggle.dto;
 
+import br.dev.wisentini.featuretoggle.exception.MissingFieldsException;
+
 import lombok.*;
+
+import org.apache.commons.lang3.StringUtils;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -18,4 +22,10 @@ public class UserCreationDTO {
     @NotBlank(message = "Password must not be blank.")
     @Size(min = 1, max = 255, message = "Password must be between 1 and 255 characters long.")
     private String password;
+
+    public void validate() {
+        if (StringUtils.isBlank(this.name) || StringUtils.isBlank(this.password)) {
+            throw new MissingFieldsException("A name and password must be provided in order to create a user.");
+        }
+    }
 }
