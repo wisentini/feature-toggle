565c139b1ba4400e0b2e780ecaf50849e25d858a
Marcos Visentini <marcosvisentini@gmail.com>
2022-12-17 14:27:38 -0300

    Create schema and data for the feature table

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -8,3 +8,7 @@ INSERT INTO "user" (name, password) VALUES ('Mateus', 'mamão2');
 INSERT INTO "user" (name, password) VALUES ('Henrique', 'melancia6');
 INSERT INTO "user" (name, password) VALUES ('Carol', 'manga10');
 INSERT INTO "user" (name, password) VALUES ('Maria', 'melão8');
+
+INSERT INTO feature (name, is_active, password) VALUES ('Pagamento à vista', TRUE, 'admin123');
+INSERT INTO feature (name, is_active, password) VALUES ('Visualização hierárquica dos dados', FALSE, 'qwerty78');
+INSERT INTO feature (name, is_active, password) VALUES ('Promoção ao usar cupom de desconto', FALSE, 'foobar99');
--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -1,3 +1,11 @@
+DROP TRIGGER IF EXISTS tr_update_updated_at_column ON public.feature;
+
+DROP TRIGGER IF EXISTS tr_update_updated_at_column ON public."user";
+
+DROP FUNCTION IF EXISTS fn_update_updated_at_column;
+
+DROP TABLE IF EXISTS feature;
+
 DROP TABLE IF EXISTS "user";
 
 CREATE TABLE IF NOT EXISTS "user" (
@@ -5,10 +13,23 @@ CREATE TABLE IF NOT EXISTS "user" (
     name       VARCHAR(255) NOT NULL,
     password   VARCHAR(255) NOT NULL,
     created_at DATE         NOT NULL DEFAULT CURRENT_DATE,
-    updated_at DATE         NULL
+    updated_at DATE             NULL,
+
+    PRIMARY KEY (user_id)
 );
 
-DROP FUNCTION IF EXISTS fn_update_updated_at_column;
+CREATE TABLE IF NOT EXISTS feature (
+    feature_id     INT          NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name           VARCHAR(255) NOT NULL,
+    is_active      BOOLEAN      NOT NULL,
+    responsible_id INT              NULL,
+    password       VARCHAR(255) NOT NULL,
+    created_at     DATE         NOT NULL DEFAULT CURRENT_DATE,
+    updated_at     DATE             NULL,
+
+    PRIMARY KEY (feature_id),
+    FOREIGN KEY (responsible_id) REFERENCES "user" (user_id)
+);
 
 CREATE OR REPLACE FUNCTION fn_update_updated_at_column() RETURNS TRIGGER AS
 '
@@ -20,8 +41,10 @@ CREATE OR REPLACE FUNCTION fn_update_updated_at_column() RETURNS TRIGGER AS
 '
 LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS tr_update_updated_at_column ON "public"."user";
-
 CREATE TRIGGER tr_update_updated_at_column
 BEFORE UPDATE ON "user"
+FOR EACH ROW EXECUTE PROCEDURE fn_update_updated_at_column();
+
+CREATE TRIGGER tr_update_updated_at_column
+BEFORE UPDATE ON feature
 FOR EACH ROW EXECUTE PROCEDURE fn_update_updated_at_column();
