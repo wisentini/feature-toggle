35eaa29d7dfadd3dc7a84a4f0d59640d84f334bb
Marcos Visentini <marcosvisentini@gmail.com>
2022-12-17 22:14:58 -0300

    Fix call to user model constructor

--- src/main/java/br/dev/wisentini/featuretoggle/mapper/UserMapper.java
+++ src/main/java/br/dev/wisentini/featuretoggle/mapper/UserMapper.java
@@ -21,6 +21,7 @@ public class UserMapper {
             userCreationDTO.getName(),
             userCreationDTO.getPassword(),
             null,
+            null,
             null
         );
     }
